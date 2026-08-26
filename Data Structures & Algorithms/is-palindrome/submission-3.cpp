7879347f863041526bb77c438cabab1ce859bcf0
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-08-26 23:28:49 +0800

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int left = 0, right = s.size() - 1;
+
+        while (left < right) {
+            if (!isalnum(s[left])) {
+                left++;
+                continue;
+            }
+
+            if (!isalnum(s[right])) {
+                right--;
+                continue;
+            }
+
+            if (toupper(s[left]) != toupper(s[right])) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+};
