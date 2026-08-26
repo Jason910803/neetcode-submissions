cbf7c6715528b7f930cd78e8e2ca77e6a8445ce1
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-08-26 19:31:44 +0800

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        string encoded;
+        for (const string& str : strs) {
+            int len = str.size();
+            encoded += to_string(len) + '#' + str;
+        }
+
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+
+        while (i < s.size()) {
+            int j = i;
+            while (s[j] != '#') {
+                j++;
+            }
+
+            string s_len = s.substr(i, j - i);
+            int len = stoi(s_len);
+            result.push_back(s.substr(j + 1, len));
+
+            i = j + len + 1; 
+        }
+
+        return result;
+    }
+};
