ac24aad11557151ce81b16ffaeef6e50ad0a7fea
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-08-26 17:06:38 +0800

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        // unordered_map<string, vector<string>> anagramMap;
+
+        // for (const string& s : strs) {
+        //     string key = s;
+        //     sort(key.begin(), key.end());
+        //     anagramMap[key].push_back(s);
+        // }
+
+        // vector<vector<string>> result;
+        // result.reserve(anagramMap.size());
+
+        // for (const auto& [key, group] : anagramMap) {
+        //     result.push_back(group);
+        // }
+
+        // return result;
+        
+        return another(strs);
+    }
+
+    vector<vector<string>> another(const vector<string>& strs) {
+        unordered_map<string, vector<string>> anagramMap;
+
+        for (const string& s : strs) {
+            vector<int> count(26, 0);
+            
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+            string key;
+            for (int c : count) {
+                key += to_string(c) + '#';
+            }
+
+            anagramMap[key].push_back(s);
+        }
+
+        vector<vector<string>> result;
+        result.reserve(anagramMap.size());
+
+        for (const auto& [key, group] : anagramMap) {
+            result.push_back(group);
+        }
+
+        return result;
+    }
+};
