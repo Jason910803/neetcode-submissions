e128b83e12f52575c5205779ba8b4f233fc4216f
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-08-28 00:56:26 +0800

    Add: longest-repeating-substring-with-replacement - submission-4

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-4.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        return sol1(s, k);
+    }
+
+    int sol1(string& s, int& k) {
+        vector<int> charCount(26);
+        int result = 0;
+        int left = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+            charCount[s[right] - 'A']++;
+            int maxLen = *max_element(charCount.begin(), charCount.end());
+
+            while (right - left + 1 - maxLen > k) {
+                charCount[s[left] - 'A']--;
+                left++;
+                maxLen = *max_element(charCount.begin(), charCount.end());
+            }
+
+            result = max(result, right - left + 1);
+        }
+
+        return result;
+    }
+};
