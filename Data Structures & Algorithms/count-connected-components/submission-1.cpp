fa17d79f3155f36164983d986158ceacb1c4150d
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-08-31 07:30:02 +0800

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.cpp
@@ -0,0 +1,44 @@
+class Solution {
+private:
+    void dfs(
+        const vector<vector<int>>& graph,
+        vector<bool>& visited,
+        int node
+    ) {
+        if (visited[node]) {
+            return;
+        }
+
+        visited[node] = true;
+
+        for (int nei : graph[node]) {
+            if (!visited[nei]) {
+                dfs(graph, visited, nei);
+            }
+        }
+    }
+
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+        vector<bool> visited(n, false);
+        vector<vector<int>> graph(n);
+
+        for (auto& edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+
+        int result = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                result++;
+                dfs(graph, visited, i);
+            }
+        }
+
+        return result;
+    }
+};
