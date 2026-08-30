5167e437772f851b94e022a355d8ab1b7f9e466c
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-08-31 05:05:54 +0800

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.cpp
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+private:
+    // original node -> clone node
+    unordered_map<Node*, Node*> cloneMap;
+
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) {
+            return nullptr;
+        }
+
+        if (cloneMap.count(node)) {
+            return cloneMap[node];
+        }
+
+        Node* copy = new Node(node->val);
+        cloneMap[node] = copy;
+
+        for (Node* nei : node->neighbors) {
+            copy->neighbors.push_back(cloneGraph(nei));
+        }
+
+        return copy;
+    }
+};
