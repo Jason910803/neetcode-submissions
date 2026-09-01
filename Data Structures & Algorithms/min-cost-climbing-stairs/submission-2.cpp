e3b2198ac6795b2cc6bde10b2f34078cf41812cd
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-09-01 19:19:26 +0800

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+private:
+    int solve(const vector<int>& cost, vector<int>& memo, int n) {
+        if (n >= cost.size()) return 0;
+
+        if (memo[n] != -1) return memo[n];
+
+        return memo[n] = cost[n] + min(solve(cost, memo, n + 1), solve(cost, memo, n + 2));
+    }
+
+public:
+    int minCostClimbingStairs(vector<int>& cost) {
+        vector<int> memo(cost.size(), -1);
+        return min(solve(cost, memo, 0), solve(cost, memo, 1));
+    }
+};
