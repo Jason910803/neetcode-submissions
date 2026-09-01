d68991946af8a7c2523cb09f0bcb277dc4532570
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-09-01 18:55:14 +0800

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    int dfs(int n, vector<int>& memo) {
+        if (n <= 2) return n;
+
+        if (memo[n] != -1) return memo[n];
+
+        return memo[n] = dfs(n - 1, memo) + dfs(n - 2, memo);
+    }
+
+public:
+    int climbStairs(int n) {
+        vector<int> memo(n + 1, -1);
+
+        return dfs(n, memo);
+    }
+};
