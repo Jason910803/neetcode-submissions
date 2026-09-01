2d0badf9b29c28c6565da76f6c2353b2f6ebe0e3
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-09-02 02:51:39 +0800

    Add: longest-palindromic-substring - submission-2

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-2.cpp
@@ -0,0 +1,35 @@
+class Solution {
+private:
+    int solve(const string& s, vector<vector<int>>& memo, int i, int j) {
+        if (i >= j) return 1;
+
+        if (memo[i][j] != -1) return memo[i][j];
+
+        if (s[i] == s[j]) {
+            return memo[i][j] = solve(s, memo, i + 1, j - 1);
+        }
+
+        return memo[i][j] = 0;
+    }
+
+public:
+    string longestPalindrome(string s) {
+        // manacher's algo is this problem
+        int n = s.size();
+        vector<vector<int>> memo(n, vector<int>(n, -1));
+
+        int len = 1;
+        int start = 0;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (solve(s, memo, i, j) && (j - i + 1) > len) {
+                    len = j - i + 1;
+                    start = i;
+                }
+            }
+        }
+
+        return s.substr(start, len);
+    }
+};
