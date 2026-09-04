cca5a251b5fa9f31a672e7c858b83f41edbd3bb1
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-09-04 23:48:29 +0800

    Add: jump-game - submission-2

---
+++ Data Structures & Algorithms/jump-game/submission-2.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    int solve(const vector<int>& nums, vector<int>& memo, int i) {
+        if (i == 0) return 1;
+        if (memo[i] != -1) return memo[i];
+
+        for (int j = i - 1; j >= 0; j--) {
+            if (solve(nums, memo, j) && j + nums[j] >= i) {
+                return memo[i] = 1;
+            }
+        }
+
+        return memo[i] = 0;
+    }
+
+public:
+    bool canJump(vector<int>& nums) {
+        // vector<int> memo(nums.size(), -1);
+        // return solve(nums, memo, nums.size() - 1) == 1;
+
+        int max_reach = 0;
+        int n = nums.size();
+
+        for (int i = 0; i < n; i++) {
+            if (i > max_reach) return false;
+
+            max_reach = max(max_reach, i + nums[i]);
+
+            if (max_reach >= n - 1) return true;
+        }
+
+        return -1;
+    }
+};
