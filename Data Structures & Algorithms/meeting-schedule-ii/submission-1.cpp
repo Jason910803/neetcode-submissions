2892f9aedd4a7cf589ab604273b838f498425d29
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-09-05 02:32:41 +0800

    Add: meeting-schedule-ii - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-1.cpp
@@ -0,0 +1,41 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+private:
+    int method1(vector<Interval>& intervals) {
+        // sorting + minHeap
+        sort(intervals.begin(), intervals.end(), [](const auto& a, const auto& b) {
+            return a.start < b.start;
+        });
+
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        int ans = 0;
+
+        for (const auto& interval : intervals) {
+            while (!minHeap.empty() && interval.start >= minHeap.top()) {
+                minHeap.pop();
+            }
+
+            minHeap.push(interval.end);
+
+            ans = max(ans, (int)minHeap.size());
+        }
+
+        return ans;
+    }
+
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        return method1(intervals);
+    }
+};
