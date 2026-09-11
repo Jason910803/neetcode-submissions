00694394f5250f5ae66c7dfe7feb468de5139690
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-09-12 06:14:24 +0800

    Add: eating-bananas - submission-3

---
+++ Data Structures & Algorithms/eating-bananas/submission-3.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    bool canFinish(const vector<int>& piles, int h, int val) {
+        int count = 0;
+
+        for (int pile : piles) {
+            count += (pile + val - 1) / val;
+            if (count > h) return false;
+        }
+
+        return true;
+    }
+
+public:
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxPile = *max_element(piles.begin(), piles.end());
+
+        int left = 1, right = maxPile + 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (canFinish(piles, h, mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return right;
+    }
+};
