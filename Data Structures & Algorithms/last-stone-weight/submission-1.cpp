cc1df949426e6585bb142ce93430e60131065f3c
Jason Cheng <138080047+Jason910803@users.noreply.github.com>
2026-09-12 14:13:45 +0800

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap(stones.begin(), stones.end());
+
+        while (maxHeap.size() >= 2) {
+            int first = maxHeap.top(); maxHeap.pop();
+            int second = maxHeap.top(); maxHeap.pop();
+
+            int remain = first - second;
+            if (remain) {
+                maxHeap.push(remain);
+            }
+        }
+
+        return maxHeap.empty() ? 0 : maxHeap.top();
+    }
+};
